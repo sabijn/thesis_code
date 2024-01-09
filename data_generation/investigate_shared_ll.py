ae10f7ec9c9276a0957314d4d22b78118574bf15
sabijn <sabijn.perdijk@gmail.com>
2024-01-09 16:27:20 +0100

    restructuring files

--- data_generation/investigate_shared_ll.py
+++ data_generation/investigate_shared_ll.py
@@ -5,10 +5,10 @@ home_path = Path("/Users/sperdijk/Documents/Master/Jaar 3/Thesis/thesis_code")
 
 # read in train_shared_levels.txt
 shared_levels = []
-with open(home_path / Path('data/train_shared_levels.txt')) as f:
+with open(home_path / Path('data/train_shared_balanced.txt')) as f:
     # shared levels
     for l in f:
-        shared_levels.extend(l.split(' '))
+        shared_levels.extend(l.strip('\n').split(' '))
 
 # count per value in shared_levels
 print(Counter(shared_levels))
