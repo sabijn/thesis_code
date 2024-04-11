3fc231f35aa8a9000b1b02b2e1df95697fb265c1
sabijn <sabijn.perdijk@gmail.com>
2024-04-11 09:05:30 +0200

    no writing trainfile

--- reduce_grammar/classes/pcfg.py
+++ reduce_grammar/classes/pcfg.py
@@ -62,8 +62,8 @@ class PCFG(Language[PCFGConfig]):
         return grammar
     
     def save_pcfg(self) -> None:
-        with open(f'{self.config.output_dir}/train_sent_{self.config.version}_{self.config.top_k}.txt', 'w') as f:
-            f.write('\n'.join(self.train_corpus))
+        # with open(f'{self.config.output_dir}/train_sent_{self.config.version}_{self.config.top_k}.txt', 'w') as f:
+        #     f.write('\n'.join(self.train_corpus))
         
         if self.config.split_ratio[1] != 0.0:
             with open(f'{self.config.output_dir}/dev_sent_{self.config.version}_{self.config.top_k}.txt', 'w') as f:
